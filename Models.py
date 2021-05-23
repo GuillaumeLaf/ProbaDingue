12f173f25cf19902e5d2ba28ba29c169bf093a15
Guillaume Lafontaine <guillaume1.lafontaine@gmail.com>
2021-05-23 15:51:58 +0200

    Commit ProbaDingue_2 and Model

---
+++ Models.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+class Model:
+    def __init__(self):
+        pass
+    
+    def fit(self):
+        pass
+    
+
+class AR(Model):
+    order:np.int8
+    phis:np.ndarray
+    sigma_e:np.float64
+    def __init__(self, order:np.int8):
+        pass
+    
+    def draw(self):
+        
+
