732018690c0d890934ee855f8e05fdc4c2d85043
Guillaume Lafontaine <guillaume1.lafontaine@gmail.com>
2021-05-23 19:26:56 +0200

    Added beginning AR model

--- Models.py
+++ Models.py
@@ -9,14 +9,26 @@ class Model:
     def fit(self):
         pass
     
+    def sample(self, n:np.int64):
+        pass
+    
 
 class AR(Model):
     order:np.int8
     phis:np.ndarray
     sigma_e:np.float64
     def __init__(self, order:np.int8):
-        pass
+        self.order = order
     
     def draw(self):
+        # Need to check if the parameters are initialised !
+        # The first element of 'prev' is the most recent obs
+        prev = np.random.normal(0,1, (self.order))
         
+        while True:
+            current_innov = np.random.normal(0,1)
+            current = self.phis @ prev + current_innov
+            yield current
+            prev = np.concatentate((current, prev[:-1]))
+            
 
